e60c37ffcaa7c10623bb5d939f1f01b917e83c00
Wung Jae Lee <wunglee2019@u.northwestern.edu>
2017-07-07 16:11:10 +0900

    Create TodoSearch component

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -1,10 +1,13 @@
 var React = require('react');
 var TodoList = require('TodoList');
 var AddTodo = require('AddTodo');
+var TodoSearch = require('TodoSearch');
 
 var TodoApp = React.createClass({
   getInitialState: function () {
     return {
+      showCompleted: false,
+      searchText: '',
       todos: [],
       itemCount: 1
     };
@@ -17,10 +20,17 @@ var TodoApp = React.createClass({
     var todo = {id: itemCount, text: todoitem};
     todos.push(todo);
   },
+  handleSearch: function (showCompleted, searchText) {
+    this.setState({
+      showCompleted: showCompleted,
+      searchText: searchText.toLowerCase()
+    })
+  },
   render: function () {
     var {todos} = this.state;
     return (
       <div className="row">
+        <TodoSearch onSearch={this.handleSearch}/>
         <div className="columns small-4">
           <TodoList todos={todos}/>
           <AddTodo onAdd={this.handleOnAdd}/>
